2da0e6f385cbca6a194c37dfe05ca37efbf807bc
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-02-27 23:12:24 +0200

    Install and setup React app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import BoardRow from "./components/BoardRow";
+
+function App() {
+  return (
+    <>
+      <BoardRow />
+      <BoardRow />
+      <BoardRow />
+    </>
+  );
+}
+
+export default App;

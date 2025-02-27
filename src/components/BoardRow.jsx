0aa433b3f4e6a976282d212812ac11139b1ecdf7
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-02-27 23:46:01 +0200

    Create an interactive component

--- src/components/BoardRow.jsx
+++ src/components/BoardRow.jsx
@@ -4,9 +4,9 @@ export default function BoardRow() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );

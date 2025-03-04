02278d4c2c3bc7f58aa606f45fbbdd78ed286f03
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-03-05 00:30:28 +0200

    Add alternating states

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 
-export default function Square(){
-    const [value, setValue] = useState(null);
-
-    function handleClick(){
-        // console.log('clicked!');
-        setValue('X')
-    }
+export default function Square({value, onSquareClick}){
 
     return(
-        <button className="square" onClick={handleClick}>{ value }</button>
-    )
+        <button 
+            className="square" 
+            onClick={onSquareClick}
+        >
+            { value }
+        </button>
+    );
 }
 
